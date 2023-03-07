3446827e2b008416b4ee81d2e03c4cb1dc547c5a
Darkpfeffer <darkpfeffer97@gmail.com>
2023-03-07 17:09:25 +0100

    added Akan Surnames

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -14,17 +14,36 @@ submitButton.addEventListener('click', (event) => {
         submitButton.click();
         
     } else {
+        appending.firstElementChild.className='';
+        appending.firstElementChild.classList.add('input-date')
         let inputDate= document.querySelector('.input-date');
+
         let dt= new Date(calendar.value);
 
-        let dayName= ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+        let dayName= ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+            'Saturday'];
         let i= dt.getDay();
 
         inputDate.classList.add(dayName[i]);
         inputDate.classList.add(personSex.value);
 
-        let dayAndSexMap= {
+        appending.firstElementChild.classList.remove('input-date')
 
+        let dayAndSexMap= {
+            'Monday_Male': 'Kwasi',
+            'Tuesday_Male': 'Kwadwo',
+            'Wednesday_Male': 'Kwabena',
+            'Thursday_Male': 'Kwaku',
+            'Friday_Male': 'Yaw',
+            'Saturday_Male': 'Kofi',
+            'Sunday_Male': 'Kwame',
+            'Monday_Female': 'Akosua',
+            'Tuesday_Female': 'Adwoa',
+            'Wednesday_Female': 'Abenaa',
+            'Thursday_Female': 'Akua',
+            'Friday_Female': 'Yaa',
+            'Saturday_Female': 'Afua',
+            'Sunday_Female': 'Ama'
         };
     }
 })
